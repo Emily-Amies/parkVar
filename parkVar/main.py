1900accc0340ff5481544c2364a7b8926c762133
Ubuntu <ubuntu@ip-10-0-0-145.eu-west-2.compute.internal>
2025-12-18 11:54:24 +0000

    Stop main.py deleting /data #57

--- parkVar/main.py
+++ parkVar/main.py
@@ -1,14 +1,27 @@
 import shutil
 from pathlib import Path
 import os
+from parkVar.utils.logger_config import logger
 
 if __name__ == '__main__':
     
     # Delete the data directory at the start of each session. If this is not 
     # done, data from the previous session will be used.
-    data_dir = '../data'
-    if os.path.isdir(data_dir):
-        shutil.rmtree(data_dir)
+    # data_dir = '../data'
+    # if os.path.isdir(data_dir):
+    #     shutil.rmtree(data_dir)
+
+    # Delete all files in the temporary data directory
+    data_dir = Path(__file__).resolve().parent.parent.parent / "data"
+
+    for item in data_dir.glob("*"):
+        try:
+            item.unlink()
+        except Exception as e:
+            logger.error(f"Failed to delete {item}: {e}")
+
+    logger.info("Data directory deleted")
+
 
     # Create a logs directory if one doesn't exist. This is because it is not
     # included in the GitHub repo
