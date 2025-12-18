1b52d6461f8d3d55c1ee1b9846293f90cc5fd1a0
growland2 <gregory.rowland@nhs.net>
2025-12-18 15:37:21 +0000

    main.py clean/fix

--- parkVar/main.py
+++ parkVar/main.py
@@ -1,15 +1,9 @@
-import shutil
-from pathlib import Path
 import os
+from pathlib import Path
+
 from parkVar.utils.logger_config import logger
 
 if __name__ == '__main__':
-    
-    # Delete the data directory at the start of each session. If this is not 
-    # done, data from the previous session will be used.
-    # data_dir = '../data'
-    # if os.path.isdir(data_dir):
-    #     shutil.rmtree(data_dir)
 
     # Delete all files in the temporary data directory
     data_dir = Path(__file__).resolve().parent.parent / "data"
@@ -22,19 +16,18 @@ if __name__ == '__main__':
 
     logger.info("Data directory deleted")
 
-
     # Create a logs directory if one doesn't exist. This is because it is not
     # included in the GitHub repo
     log_dir = '../logs'
     if not os.path.isdir(log_dir):
         os.makedirs(log_dir)
 
-    # This is here (not at the top) because the logs directory needs to be 
+    # This is here (not at the top) because the logs directory needs to be
     # made before the import
     from parkVar.modules.flask_app import app
 
     # Run the app
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
 
     # lsof -i :5000
